45179ec62ee066b0baabb65d5646cd99760a706e
Besfort Mehmeti <hello@besfort.info>
2024-10-21 10:04:40 +0200

    feat: add static routes for setupRequest and login methods

---
+++ routes/abeta.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use AbetaIO\Laravel\Http\Controllers;
+
+Route::prefix(config('abeta.routes.prefix'))
+    ->name('abeta.')
+    ->group(function () {
+        Route::post('setup-request', [Controllers\PunchOutController::class, 'setupRequest'])->name('setupRequest');
+        Route::get('login', [Controllers\LoginController::class, 'login'])->name('login');
+    });
