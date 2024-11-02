91b65219bfd7d75578e8511b09a46d01ba465d65
Besfort Mehmeti <hello@besfort.info>
2024-11-02 13:13:58 +0100

    set web middleware for routes

--- routes/abeta.php
+++ routes/abeta.php
@@ -5,6 +5,7 @@ use AbetaIO\Laravel\Http\Controllers;
 
 Route::prefix(config('abeta.routes.prefix'))
     ->name('abeta.')
+    ->middleware(['web'])
     ->group(function () {
         Route::post('setup-request', [Controllers\PunchOutController::class, 'setupRequest'])->name('setupRequest');
         Route::get('login', [Controllers\LoginController::class, 'login'])->name('login');
