31a061cf8f4701c172d75e86fa80f9f72978d7d9
Besfort Mehmeti <hello@besfort.info>
2024-11-04 20:35:26 +0100

    refactor: apply web middleware only to login route for session handling

--- routes/abeta.php
+++ routes/abeta.php
@@ -5,8 +5,7 @@ use AbetaIO\Laravel\Http\Controllers;
 
 Route::prefix(config('abeta.routes.prefix'))
     ->name('abeta.')
-    ->middleware(['web'])
     ->group(function () {
         Route::post('setup-request', [Controllers\PunchOutController::class, 'setupRequest'])->name('setupRequest');
-        Route::get('login', [Controllers\LoginController::class, 'login'])->name('login');
+        Route::middleware(['web'])->get('login', [Controllers\LoginController::class, 'login'])->name('login');
     });
