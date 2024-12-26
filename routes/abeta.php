c416c031081accfd6a20232b602774628b3010d6
Besfort Mehmeti <hello@besfort.info>
2024-12-26 18:51:08 +0100

    Fix typo in route definition

--- routes/abeta.php
+++ routes/abeta.php
@@ -9,5 +9,5 @@ Route::prefix(config('abeta.routes.prefix'))
     ->group(function () {
         Route::post('setup-request', [Controllers\PunchOutController::class, 'setupRequest'])->name('setupRequest');
         Route::get('login', [Controllers\LoginController::class, 'login'])->name('login');
-        Route::post('order-confrimation', [Controllers\OrderController::class, 'confirm'])->name('order.confirm');
+        Route::post('order-confirmation', [Controllers\OrderController::class, 'confirm'])->name('order.confirm');
     });
